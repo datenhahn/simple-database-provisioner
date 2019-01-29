d2172d2da8db19f2da4d2020c084dc26aea59de4
Jonas Hahn <jonas.hahn@datenhahn.de>
2019-01-29 15:13:10 +0100

    - fixes issue #3 : drop all connections to db before deleting - bump versions

---
+++ build_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+APPVERSION=v0.1.1
+
+docker build . -t ecodia/simple-database-provisioner
+
+docker tag ecodia/simple-database-provisioner ecodia/simple-database-provisioner:$APPVERSION
